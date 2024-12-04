7bacb67eaecc7fc00f1713fda94ffe6e56b53881
mmtg445 <mmtg445@gmail.com>
2024-12-05 00:01:32 +0600

    Update config.py

--- helper/config.py
+++ helper/config.py
@@ -3,11 +3,11 @@
 from . import *
 
 try:
-    APP_ID = config("APP_ID", cast=int)
-    API_HASH = config("API_HASH")
-    BOT_TOKEN = config("BOT_TOKEN")
-    OWNER = config("OWNER_ID", default=8102446291, cast=int)
-    LOG = config("LOG_CHANNEL", cast=int)
+    APP_ID = config("APP_ID", "21188057")
+    API_HASH = config("API_HASH", "8564fab8db759bb04b1907bd12ed98ef")
+    BOT_TOKEN = config("BOT_TOKEN", "8169313125:AAHQ6ELfV2kisRxB3XHdIOwlDaSonZfzAb0")
+    OWNER = config("OWNER_ID", "8102446291")
+    LOG = config("LOG_CHANNEL", "-1002289024376")
 except Exception as e:
     LOGS.info("Environment vars Missing")
     LOGS.info("something went wrong")
